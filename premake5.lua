0bdc3cadaad87fb522e123764a7ffa2a7a672401
wij <41879253+wijiler@users.noreply.github.com>
2024-10-06 16:17:54 -0400

    Init

---
+++ premake5.lua
@@ -0,0 +1,53 @@
+workspace "Wrenderer"
+project "Wrengine"
+    language "C++"
+    targetname "Wrengine"
+    architecture "x64"
+    outputdir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+    kind "ConsoleApp"
+    toolset "clang"
+    cppdialect "C++17"
+
+    targetdir("%{wks.location}/Binaries/" .. outputdir .. "/%{prj.name}")
+    objdir("%{wks.location}/Binaries/Intermediates/" .. outputdir .. "/%{prj.name}")
+    files { "**.hpp", "**.cpp" }
+    libdirs { "./libs/" }
+    includedirs { "./include/" }
+    includedirs { "./include/libs/" }
+    includedirs { os.getenv("VULKAN_SDK") .. "/Include" }
+    buildoptions { "-Wextra -Wall" }
+    links { "vulkan-1", "glfw3" }
+    links { "Wrenderer" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+        optimize "On"
+    filter "configurations:Release"
+        optimize "On"
+
+    filter "system:windows"
+        links { "user32", "msvcrt", "gdi32", "shell32", "libcmt" }
+        defines { "VK_USE_PLATFORM_WIN32_KHR" }
+
+    filter "system:linux"
+        defines { "VK_USE_PLATFORM_XLIB_KHR" }
+
+    filter "action:gmake"
+    prebuildcommands {
+
+        "mkdir -p" .. " %{wks.location}/Binaries/",
+        "mkdir -p" .. " %{wks.location}/Binaries/Intermediates/",
+
+        "mkdir -p" .. " %{wks.location}/Binaries/" .. outputdir,
+        "mkdir -p" .. " %{wks.location}/Binaries/Intermediates/" .. outputdir,
+    }
+    filter "not action:gmake"
+        prebuildcommands {
+            "{MKDIR}" .. " %{wks.location}/Binaries/",
+            "{MKDIR}" .. " %{wks.location}/Binaries/Intermediates/",
+
+            "{MKDIR}" .. " %{wks.location}/Binaries/" .. outputdir,
+            "{MKDIR}" .. " %{wks.location}/Binaries/Intermediates/" .. outputdir,
+        }
+include "Wrenderer/premake5.lua"
